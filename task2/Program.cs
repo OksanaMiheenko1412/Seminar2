2a96100326042242e33541280aa4660e1eb4b18e
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-27 15:01:29 +0700

    / add code task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 11
+Console.Clear();
+
+int n = new Random().Next(100, 1000); // [100, 999]
+Console.WriteLine($"Случайное число: {n}");
+int n1 = n / 100; // количество сотен
+int n3 = n % 10; // количество единиц (последняя цифра числа)
+Console.Write(n1 * 10 + n3);
