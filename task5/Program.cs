54ccb8a8282640bb0a14ea7ab034ad4a04059416
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-27 18:17:58 +0700

    / add code task5

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите 2-ое  число");
+int b = Convert.ToInt32(Console.ReadLine()); 
+if (a * a == b || b * b == a )
+    Console.Write("yes");
+else
+    Console.Write("no");    
